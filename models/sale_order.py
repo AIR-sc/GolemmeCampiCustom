577a02a4618862ac49ff653373c159df95813803
Simone C <simone.cuffaro@air-srl.com>
2021-03-29 09:21:28 +0200

    Campo testo_scotno_detrazione in html

--- models/sale_order.py
+++ models/sale_order.py
@@ -4,4 +4,4 @@ from odoo import api, fields, models, _
 class sale_order_template(models.Model):
     _inherit = "sale.order"
 
-    testo_sconto_detrazione = fields.Text('Testo sconto detrazione')
+    testo_sconto_detrazione = fields.Html('Testo sconto detrazione')
